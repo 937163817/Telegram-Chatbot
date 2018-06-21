0e1328a0432c1d99abfac9d2aa3da3c610a61bc4
tottokoooo <38970897+tottokoooo@users.noreply.github.com>
2018-06-21 20:34:49 +0800

    Add files via upload

---
+++ mic.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+r=sr.Recognizer() 
+with sr.Microphone() as source:
+    print("Please wait. Calibrating microphone...") 
+    r.adjust_for_ambient_noise(source, duration=5) 
+    print("Say something!")
+    audio=r.listen(source)
+
+try:
+    print("Google Speech Recognition thinks you said:")
+    print(r.recognize_google(audio, language="zh-TW"))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("No response from Google Speech Recognition service: {0}".format(e))
