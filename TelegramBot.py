9e143322dbc980e166a8340cd61d59d67600d768
937163817 <937163817@qq.com>
2018-06-22 11:23:19 +0800

    Update Telegram.py
    add movie ptt

--- TelegramBot.py
+++ TelegramBot.py
@@ -39,6 +39,93 @@ token = '606184614:AAEaapXpdRswzj3L--saKbCWr7K2kzyIcn0' #改成我們的token
 magnito_bot = BotHandler(token) #Your bot's name
 
 
+def get_web_page(url):
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        print('Invalid url:', resp.url)
+        return None
+    else:
+        return resp.text
+
+def get_movies(dom):
+    soup = BeautifulSoup(dom, 'html5lib')
+    movies = []
+    rows = soup.find_all('div', 'release_info_text')
+    for row in rows:
+        movie = dict()
+        #movie['推薦度'] = row.find('div', 'leveltext').span.text.strip()
+        movie['ch_name'] = row.find('div', 'release_movie_name').a.text.strip()
+        #movie['eng_name'] = row.find('div', 'release_movie_name').find('div', 'en').a.text.strip()
+        #movie['release_date'] = get_date(row.find('div', 'release_movie_time').text)
+        trailer_a = row.find_next_sibling('div', 'release_btn color_btnbox').find_all('a')[1]
+        movie['trailer_url'] = trailer_a['href'] if 'href' in trailer_a.attrs.keys() else ''
+        movies.append(movie)
+    return movies
+
+def get_date(date_str):
+    pattern = '\d+-\d+-\d+'
+    match = re.search(pattern, date_str)
+    if match is None:
+        return date_str
+    else:
+        return match.group(0)
+
+def get_movie_id(url):
+    try:
+        movie_id = url.split('.html')[0].split('-')[-1]
+    except:
+        movie_id = url
+    return movie_id
+
+def get_web_page(url):
+    resp = requests.get(
+        url=url,
+        cookies={'over18': '1'}
+    )
+    if resp.status_code != 200:
+        print('Invalid url:', resp.url)
+        return None
+    else:
+        return resp.text
+
+def get_articles(dom, date):
+    soup = BeautifulSoup(dom, 'html5lib')
+
+    paging_div = soup.find('div', 'btn-group btn-group-paging')
+    prev_url = paging_div.find_all('a')[1]['href']
+
+    articles = []
+    divs = soup.find_all('div', 'r-ent')
+    for d in divs:
+        if d.find('div', 'date').text.strip() == date:
+            push_count = 0
+            push_str = d.find('div', 'nrec').text
+            if push_str:
+                try:
+                    push_count = int(push_str)
+                except ValueError:
+                    if push_str == 'burst':
+                        push_count = 99
+                    elif push_str.startswith('X'):
+                        push_count = -10
+
+            if d.find('a'):
+                href = d.find('a')['href']
+                title = d.find('a').text
+                author = ''
+                articles.append({
+                    'title':title,
+                    'href':'https://www.ptt.cc'+href,
+                    'push_count': push_count,
+                })
+    return articles, prev_url
+
+def get_author_ids(posts, pattern):
+    ids = set()
+    for post in posts:
+        if pattern in post['author']:
+            ids.add(post['author'])
+    return ids
 
 def main():
     new_offset = 0
@@ -195,7 +282,36 @@ def main():
                         break
                     except sr.RequestError as e:
                         magnito_bot.send_message(first_chat_id, 'No response from Google Speech Recognition service: {0}'.format(e))
-                        break	
+                        break
+			
+                elif first_chat_text == 'movie':
+                    page = get_web_page('https://tw.movies.yahoo.com/movie_thisweek.html')
+                    if page:
+                        movies = get_movies(page)
+                    for movie in movies:
+                        magnito_bot.send_message(first_chat_id,movie['ch_name'])
+                        magnito_bot.send_message(first_chat_id,movie['trailer_url'])
+                    new_offset = first_update_id + 1
+		
+                elif first_chat_text == 'ptt':
+                    current_page = get_web_page('https://www.ptt.cc/bbs/Gossiping/index.html')
+                    if current_page:
+                        articles = []
+                        today = time.strftime("%m/%d").lstrip('0')
+                        current_articles, prev_url = get_articles(current_page, today)
+                        while current_articles:
+                            articles += current_articles
+                            current_page = get_web_page('https://www.ptt.cc' + prev_url)
+                            current_articles, prev_url = get_articles(current_page, today)
+
+                            #print('There are today', len(articles), 'Article')
+                            threshold = 50
+                            #print('popular articles(> %d Push):' % (threshold))
+                        for a in articles:
+                            if int(a['push_count']) > threshold:
+                                magnito_bot.send_message(first_chat_id,a['href'])
+                    new_offset = first_update_id + 1
+                   
                 else :
                     magnito_bot.send_message(first_chat_id, 'Enter another instruction\n╰(*°▽°*)╯'+first_chat_name)
                     new_offset = first_update_id + 1 
